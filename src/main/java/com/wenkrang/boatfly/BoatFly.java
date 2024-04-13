e517944f041a51e882c25760f72bc6f4d982fe8b
Wenkrang <2934356978@qq.com>
2024-04-13 09:43:25 +0800

    upgrade

--- src/main/java/com/wenkrang/boatfly/BoatFly.java
+++ src/main/java/com/wenkrang/boatfly/BoatFly.java
@@ -1,6 +1,5 @@
 package com.wenkrang.boatfly;
 
-import com.wenkrang.boatfly.BootLoader.BoatFlyBoot;
 import com.wenkrang.boatfly.Data.MainData;
 import com.wenkrang.boatfly.Loader.LoadCommand;
 import com.wenkrang.boatfly.Loader.LoadEvent;
@@ -9,16 +8,38 @@ import com.wenkrang.boatfly.event.GUI.book.PlayerInteract;
 import com.wenkrang.boatfly.lib.ConsoleLoger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
+import static org.bukkit.Bukkit.getServer;
+
 
 public final class BoatFly extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        MainData.PluginName = getName();
         MainData.plugin = getPlugin(BoatFly.class);
         MainData.PluginFile = getFile();
         // Plugin startup logic
-        BoatFlyBoot.boot();
+        //启动插件,显示启动信息
+        getServer().getConsoleSender().sendMessage("    ____              __  ________     ");
+        getServer().getConsoleSender().sendMessage("   / __ )____  ____ _/ /_/ ____/ /_  __");
+        getServer().getConsoleSender().sendMessage("  / __  / __ \\/ __ `/ __/ /_  / / / / /");
+        getServer().getConsoleSender().sendMessage(" / /_/ / /_/ / /_/ / /_/ __/ / / /_/ / ");
+        getServer().getConsoleSender().sendMessage("/_____/\\____/\\__,_/\\__/_/   /_/\\__, /  ");
+        getServer().getConsoleSender().sendMessage("                              /____/   ");
+        try {
+            //这里将启动分为3个部分，1.加载指令 2.加载事件 3.加载合成
+            LoadCommand.run();
+            LoadEvent.run();
+            LoadRecipe.run();
+            //检测服务器版本，动态修补兼容问题
+            if (PlayerInteract.isBelow1_20_4()) {
+                //提醒腐竹更新服务器
+                ConsoleLoger.info("您的服务器版本低于1.20.4,部分功能可能无法正常使用");
+            }
+            //加载完成
+            getServer().getConsoleSender().sendMessage("§9§l[*] §r加载完毕,当前版本 : 1.5.3a");
+        } catch (Exception e) {}
     }
 
     @Override
--- src/main/java/com/wenkrang/boatfly/BootLoader/SetupInformation.java
+++
@@ -1,7 +0,0 @@
-package com.wenkrang.boatfly.BootLoader;
-
-public class SetupInformation {
-    public static String LastestName = "BoatFly-1.5.3a";
-    public static String LastestNumber = "1";
-    public static String LastestVersion = "1.5.3a";
-}
