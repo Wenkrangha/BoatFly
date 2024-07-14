947478dd788909fc4247cb819c2732e919e7ca89
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2024-07-14 20:38:30 +0800

    Modify | 返回List

--- src/main/java/com/wenkrang/boatfly/lib/ShapedRecipeUtil.java
+++ src/main/java/com/wenkrang/boatfly/lib/ShapedRecipeUtil.java
@@ -3,12 +3,14 @@ package com.wenkrang.boatfly.lib;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("deprecated")
+@SuppressWarnings("deprecation")
 public class ShapedRecipeUtil {
-    public static ItemStack[] getItemStacks(ShapedRecipe recipe) {
-        ItemStack[] itemStacks;
+    public static List<ItemStack> getItemStacks(ShapedRecipe recipe) {
+        List<ItemStack> itemStacks = new ArrayList<>();
         Map<ItemStack, Integer> map = new java.util.HashMap<>(Map.of());
         final String[] shape = recipe.getShape();
         for (int i = 0; i < shape.length; i++) {
@@ -18,11 +20,10 @@ public class ShapedRecipeUtil {
             }
         }
         map.forEach((k, v) -> map.merge(k, v, Integer::sum));
-        itemStacks = new ItemStack[map.size()];
         for (Map.Entry<ItemStack, Integer> entry : map.entrySet()) {
             var itemStack = entry.getKey();
             itemStack.setAmount(entry.getValue());
-            itemStacks[entry.getValue()] = itemStack;
+            itemStacks.add(itemStack);
         }
 
         return itemStacks;
